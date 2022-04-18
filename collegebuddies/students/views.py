94ca5d967166eb9925d556d00cf714c84ed7b6a0
EC2 Default User <ec2-user@ip-172-31-39-51.eu-west-2.compute.internal>
2022-04-18 09:09:53 +0000

    updated students/view.py

--- collegebuddies/students/views.py
+++ collegebuddies/students/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("You're at the College Buddies index.")
+    return HttpResponse("********* Welcome to College Buddies. *************")
